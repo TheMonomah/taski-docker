5ffc2bfa6deef9dd444ad40ff128585a553bea0f
TheMonomah <the.v.yakimenko@gmail.com>
2023-07-13 18:02:40 +0300

    Add workflow2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,20 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default = 'django-insecure-hj(r17^!qxjrzvc9weeh(ck=@dq%gz9m4tbvcr7$-$5(k_m)4x')
 
-DEBUG = True
+DEBUG = os.getenv('DEBAG', default = False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.65.213', '127.0.0.1', 'localhost', 'taskimono26.ddns.net']
 
 
 # Application definition
